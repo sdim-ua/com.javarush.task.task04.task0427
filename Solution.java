7301aa21b114e4c0afdd30303fd4adbf576082a8
sdim-ua <ingersol@yandex.ua>
2019-01-22 23:47:09 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,37 @@
+/* 
+Описываем числа
+
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int numb = Integer.parseInt(reader.readLine());
+        
+        if(numb < 999 && numb > 0 && numb < 10 && numb % 2 == 0 ){
+            System.out.println("четное однозначное число");
+        }
+        else if(numb < 999 && numb > 0 && numb < 10 && numb % 2 != 0 ){
+            System.out.println("нечетное однозначное число");
+        }
+         else if(numb < 999 && numb > 0 && numb < 100 && numb % 2 == 0 ){
+            System.out.println("четное двузначное число");
+        }
+         else if(numb < 999 && numb > 0 && numb < 100 && numb % 2 != 0 ){
+            System.out.println("нечетное двузначное число");
+        }
+        else if(numb < 999 && numb > 0 && numb < 1000 && numb % 2 == 0 ){
+            System.out.println("четное трехзначное число");
+        }
+        else if(numb < 999 && numb > 0 && numb < 1000 && numb % 2 != 0 ){
+            System.out.println("нечетное трехзначное число");
+        }
+         
+    }
+    
+}
